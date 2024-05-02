b9be3e0227af05cddf5bb7814fd38186ddbd6818
kubuszi <kubushii2@wp.pl>
2024-05-02 11:34:48 +0200

    Add the Scoreboard

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,8 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
+
 screen.listen()
 screen.onkey(player.go_up, "Up")
 
@@ -23,7 +26,11 @@ while game_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_on = False
+            scoreboard.game_over()
 
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_level()
+
+screen.exitonclick()
